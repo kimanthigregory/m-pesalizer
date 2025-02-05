f88e2f0101a86bd45f86fa639fb2545973268b02
jocha college <gregorykimanthi100@gmail.com>
2025-02-05 23:03:57 +0300

    make a card component

---
+++ client/src/components/cardData.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    title: "upload you data",
+    description: " upload your pdf file containing statement",
+    image: "upload.svg",
+  },
+  {
+    id: 2,
+    title: "data processing",
+    description: "after upload you data is processed",
+    image: "visualize.svg",
+  },
+  {
+    id: 3,
+    title: "analytics",
+    description: "get beutifully presented analysis",
+    image: "ideas.svg",
+  },
+];
--- client/src/routes/landingPage.jsx
+++ client/src/routes/landingPage.jsx
@@ -1,5 +1,7 @@
 import "../index.css";
 import heroImage from "../assets/hero-image.svg";
+import Card from "../components/card";
+import cardData from "../components/cardData";
 
 export default function LandingPage() {
   return (
@@ -21,48 +23,10 @@ export default function LandingPage() {
           />
         </div>
       </section>
-      <section>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-            <img
-              class="rounded-t-lg"
-              src="/docs/images/blog/image-1.jpg"
-              alt=""
-            />
-          </a>
-          <div class="p-5">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-          </div>
-        </div>
+      <section className="flex flex-col-reverse md:flex-row gap-3 :flex-col">
+        {cardData.map((data) => {
+          return <Card key={data.id} item={data} />;
+        })}
       </section>
     </>
   );
